92730cca4b6a56b1063d20e1ba1c0179d38de89e
Lucas Duailibe <lucasds@gmail.com>
2017-09-11 11:47:09 -0300

    Initial commit

---
+++ Formula/arcanist.rb
@@ -0,0 +1,28 @@
+class Arcanist < Formula
+  desc "Command-line interface for Phabricator"
+  homepage "https://secure.phabricator.com/book/phabricator/article/arcanist/"
+  url "https://github.com/phacility/arcanist.git",
+    :revision => "cbc785ddce71be073f536c1faea8c231e495d5df"
+  version "20170911"
+
+  resource "libphutil" do
+    url "https://github.com/phacility/libphutil.git",
+      :revision => "ceeaf5ebea1c57c554eff0a2f131a21072f2948d"
+  end
+
+  def install
+    libexec.install Dir["*"]
+
+    resource("libphutil").stage do
+      (buildpath/"libphutil").install Dir["*"]
+    end
+
+    prefix.install Dir["*"]
+
+    bin.install_symlink libexec/"bin/arc" => "arc"
+  end
+
+  test do
+    system "#{bin}/arc", "help"
+  end
+end
---
+++ README.md
@@ -0,0 +1,16 @@
+Geekie Homebrew Tap
+===================
+
+This repository contains some tools we use for development at Geekie that can be more easily installed with [Homebrew](https://brew.sh).
+
+To install a package:
+
+```
+brew tap geekie/tap
+brew install arcanist
+```
+
+Available packages:
+-------------------
+
+* [`arcanist`](https://github.com/phacility/arcanist) - Command line interface for Phabricator
