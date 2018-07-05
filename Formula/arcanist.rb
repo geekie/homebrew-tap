04d2d834e1b7a39bd0cb12f90126265e98915812
Lucas Duailibe <lucasds@gmail.com>
2018-07-05 15:54:03 -0300

    Update arcanist

--- Formula/arcanist.rb
+++ Formula/arcanist.rb
@@ -3,15 +3,11 @@ class Arcanist < Formula
   homepage "https://secure.phabricator.com/book/phabricator/article/arcanist/"
   url "https://github.com/phacility/arcanist.git",
     :revision => "cbc785ddce71be073f536c1faea8c231e495d5df"
-  version "20170911"
+  version "201825"
 
   resource "libphutil" do
     url "https://github.com/phacility/libphutil.git",
-      :revision => "ceeaf5ebea1c57c554eff0a2f131a21072f2948d"
-  end
-
-  resource "geekie-extensions" do
-    url "https://github.com/geekie/arc-extensions.git"
+      :revision => "47c97f0c48429a25f35ca9b515c7b9e15889d77f"
   end
 
   def install
